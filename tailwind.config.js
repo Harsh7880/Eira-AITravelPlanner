fb5981989d9b7591ea2521e8a60fc343c071fd62
harsh7880 <vijayk8478@gmail.com>
2024-12-08 12:54:49 +0530

    Started Adding theme swicthcing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 export default {
     darkMode: ["class"],
     content: [
@@ -59,5 +61,5 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 }
